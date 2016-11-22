3d6dc89b4b61c8f0b5a510ceabedd800133addc8
Andreas Plüss <andreas@qlu.ch>
2016-11-22 15:21:16 +0100

    KISS Removed orientation check and put grid layout into scroll layout.

--- BoilerPlate/BoilerPlate/Views/GridXamlPage.xaml.cs
+++ BoilerPlate/BoilerPlate/Views/GridXamlPage.xaml.cs
@@ -15,20 +15,6 @@ namespace BoilerPlate.Views
         protected override void OnSizeAllocated(double width, double height)
         {
             base.OnSizeAllocated(width, height);
-            if (width > height)
-            {
-                var gridBounds = GridWrapper.Bounds;
-                gridBounds.Width = Width;
-                gridBounds.Height = Width;
-                GridWrapper.LayoutTo(gridBounds, 250, Easing.SinInOut);
-            }
-            else
-            {
-                var gridBounds = GridWrapper.Bounds;
-                gridBounds.Width = Width;
-                gridBounds.Height = Width;
-                GridWrapper.LayoutTo(gridBounds, 250, Easing.SinInOut);
-            }
         }
     }
 }
