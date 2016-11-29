09f074d271d9cb0fdba90b198318f369d7c2377a
Andreas Plüss <andreas@qlu.ch>
2016-11-29 17:45:55 +0100

    set up local notifications

---
+++ BoilerPlate/BoilerPlate.iOS/Helpers/NotifyService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BoilerPlate.Helper;
+using Foundation;
+using UIKit;
+
+namespace BoilerPlate.iOS.Helpers
+{
+    class NotifyService : INotifyService
+    {
+        public void Remind(DateTime dateTime, string title, string message)
+        {
+            var notification = new UILocalNotification();
+
+            //---- set the fire date (the date time in which it will fire)
+            //notification.FireDate = (NSDate) dateTime;
+            notification.FireDate = NSDate.FromTimeIntervalSinceNow(60);
+
+            //---- configure the alert stuff
+            notification.AlertAction = title;
+            notification.AlertBody = message;
+
+            //---- modify the badge
+            notification.ApplicationIconBadgeNumber = 1;
+
+            //---- set the sound to be the default sound
+            notification.SoundName = UILocalNotification.DefaultSoundName;
+
+            //---- schedule it
+            UIApplication.SharedApplication.ScheduleLocalNotification(notification);
+        }
+    }
+}
---
+++ BoilerPlate/BoilerPlate/Helper/INotifyService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BoilerPlate.Helper
+{
+    public interface INotifyService
+    {
+        void Remind(DateTime dateTime, string title, string message);
+    }
+}
